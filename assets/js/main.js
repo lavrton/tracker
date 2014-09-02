ddfed16313de8cd594169ff6401c4472fb29c854
Лаврёнов Антон <lavrton@gmail.com>
2014-09-02 18:28:09 +0800

    Deleting question, table for answers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,69 @@
 var QuestionList = React.createClass({
+    displayName: 'QuestionList',
     render : function() {
+        var that = this;
         var createItem = function(question) {
-            return React.DOM.li({key : question.id}, question.title);
+            return AnswerComponent({
+                question : question,
+                onDelete : that.props.onDelete
+            });
         };
-        return React.DOM.ul(null, this.props.questions.map(createItem));
+        return React.DOM.div(null,
+            React.DOM.h2(null, "Your data"),
+            React.DOM.ul(null, this.props.questions.map(createItem))
+        );
+    }
+});
+
+var AnswerComponent = React.createClass({
+    displayName: 'AnswerComponent',
+    onDeleteClick : function () {
+        var that = this;
+        io.socket.delete('/question', { id : this.props.question.id}, function () {
+            that.props.onDelete(that.props.question);
+        });
+    },
+    render : function() {
+        var answers = [];
+        for (var i = 0; i< 20; i++) {
+            answers.push(Math.round(Math.random()) === 1)
+        }
+        var createButton = function(value) {
+            return React.DOM.button({
+                    className : 'topcoat-icon-button--quiet'
+                },
+                React.DOM.span({
+                    className : 'topcoat-icon',
+                    style : {
+                        'background-color' : (value ? '#45F568' : '#A5A7A7')
+                    }
+                }));
+        };
+
+        return React.DOM.li({
+                key : this.props.question.id
+            },
+            answers.map(createButton),
+            this.props.question.title + ' ',
+            React.DOM.button({
+                    className : 'topcoat-icon-button',
+                    onClick : this.onDeleteClick
+                },
+                React.DOM.span({
+                    style : {
+                        'color' : '#F23F3F'
+                    }
+                }, 'delete')
+            )
+        );
     }
 });
 
 var App = React.createClass({
     getInitialState: function() {
-        return {questions: []};
+        return {
+            questions: []
+        };
     },
     componentDidMount: function() {
         var that = this;
@@ -27,10 +81,30 @@ var App = React.createClass({
         this.state.questions = this.state.questions.concat(question);
         this.setState(this.state);
     },
+    onDelete : function(question) {
+        var index = this.state.questions.indexOf(question);
+        if (index > -1) {
+            this.state.questions.splice(index, 1);
+        } else {
+            throw "Can't delete question from state. Has no such question."
+        }
+        this.setState(this.state);
+    },
     render: function() {
         return React.DOM.div(null,
-                AddQuestion({onAdd : this.onAdd}),
-                QuestionList({questions : this.state.questions})
+                React.DOM.div({
+                        className : 'grid-25'
+                    },
+                    AddQuestion({onAdd : this.onAdd})
+                ),
+                React.DOM.div({
+                        className : 'grid-75'
+                    },
+                    QuestionList({
+                        questions : this.state.questions,
+                        onDelete : this.onDelete
+                    })
+                )
             );
     }
 });
@@ -51,6 +125,7 @@ var AddQuestion = React.createClass({displayName: 'AddQuestion',
     },
     render: function() {
         return React.DOM.div(null,
+            React.DOM.h2(null, "Add question"),
             React.DOM.input({
                 type: "text",
                 className: "topcoat-text-input question",
@@ -70,7 +145,7 @@ var AddQuestion = React.createClass({displayName: 'AddQuestion',
 
 React.renderComponent(
     App(),
-    document.getElementById('questions')
+    document.getElementById('content')
 );
 
 
